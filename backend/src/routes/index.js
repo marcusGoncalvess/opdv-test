05b620b824a024cc1cd1ce3ff58cf91352325dd6
Marcus Gonçalves <marcus.goncalvezz@outlook.com>
2020-11-09 19:15:29 -0300

    update route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,19 +4,11 @@ import FormatDataOfAPI from '../services/FormatDataOfAPI.js';
 
 const routes = Router();
 
-routes.get('/menu', async (request, response) => {
-  const {
-    restaurantCode,
-    latitude,
-    longitude,
-    scheduling_time,
-    secret_key,
-    account_id,
-    access_key,
-  } = request.body;
+routes.get('/menu/:restaurantCode', async (request, response) => {
+  const { restaurantCode } = request.params;
 
   const responseAPI = await axios.get(
-    `https://wsloja.ifood.com.br/ifood-ws-v3/restaurants/${restaurantCode}/menu?latitude=${latitude}&longitude=${longitude}&scheduling_time=${scheduling_time}&x-ifood-session-id=b22fbbed-21a6-454d-a521-d1dc68bbbcd4&secret_key=${secret_key}&account_id=${account_id}&access_key=${access_key}`,
+    `https://wsloja.ifood.com.br/ifood-ws-v3/restaurants/${restaurantCode}/menu?latitude=-30.0253397&longitude=-51.1967082&scheduling_time=1603467600000&x-ifood-session-id=b22fbbed-21a6-454d-a521-d1dc68bbbcd4&secret_key=9ef4fb4f-7a1d-4e0d-a9b1-9b82873297d8&account_id=26e25614-6e13-4c77-8761-2ebfc97554e0&access_key=69f181d5-0046-4221-b7b2-deef62bd60d5`,
   );
 
   const formatData = new FormatDataOfAPI();
